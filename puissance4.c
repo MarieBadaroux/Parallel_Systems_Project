372908d88af1b07ac7dfb9f8fed60e1f51f770cc
Marie Badaroux <marie.badaroux@grenoble-inp.org>
2019-11-22 15:12:24 +0100

    Implementation play method

--- puissance4.c
+++ puissance4.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "puissance4.h"
 
 #define NB_COLUMN 7
 #define NB_LINE 6
+#define PLAYER1 1
+#define PLAYER2 2
 
-//static struct position player1_positions[22];
-//static struct position player2_positions[22];
+static char grid[NB_LINE][NB_COLUMN];
 
 
 void display_grid(void) {
@@ -32,7 +34,11 @@ void display_grid(void) {
 				printf("|");
 				continue;
 			}
-			printf("   |");
+			if (isalpha(grid[i][j-1])) {
+				printf(" %c |", grid[i][j-1]);
+			} else {
+				printf("   |");
+			}
 		}
 		printf("\n");
 		for (int k = 0; k < NB_COLUMN + 1; k++) {    
@@ -47,8 +53,61 @@ void display_grid(void) {
 }
 
 
+void play(int column, int player) {
+	// If the column is full
+	if (isalpha(grid[0][column-1])) {
+		return;
+	}
+
+	for (int i = 0; i < NB_LINE; i++) {
+		if (isalpha(grid[i][column-1])) {
+			if (player == 1) {
+				grid[i-1][column-1] = 'O';
+			} else {
+				grid[i-1][column-1] = 'X';
+			}
+			return;
+		}
+		// If we play on the last line
+		if (i == NB_LINE - 1) {
+			if (player == 1) {
+				grid[i][column-1] = 'O';
+			} else {
+				grid[i][column-1] = 'X';
+			}
+			return;
+		}
+	}
+}
+
+
+int win_horizontal(int line, int column) {
+	return 0;
+}
+
 
 int main(void) {
-	display_grid();
+	int player1_column;
+	int player2_column;
+
+	while(1) {
+		printf("Player 1 : ");
+		scanf("%d", &player1_column);
+		while (player1_column < 1 || player1_column > 7) {
+			printf("Player 1 : Error enter again the column = ");
+			scanf("%d", &player1_column);
+		}
+		play(player1_column, PLAYER1);
+		display_grid();
+
+		printf("Player 2 : ");
+		scanf("%d", &player2_column);
+		while (player2_column < 1 || player2_column > 7) {
+			printf("Player 2 : Error enter again the column = ");
+			scanf("%d", &player2_column);
+		}
+		play(player2_column, PLAYER2);
+		display_grid();
+	}
 	return EXIT_SUCCESS;
 }
--- puissance4.h
+++ puissance4.h
@@ -1,6 +1,5 @@
-struct position {
-	int line;
-	int column;
-};
-
 void display_grid(void);
+void play(int column, int player);
+int win_horizontal(int line, int column);
+int win_vertical(int line, int column);
+int win_diagonal(int line, int column);
